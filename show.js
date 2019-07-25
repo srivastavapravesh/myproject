fec42c12c46da0f4655f0cfcf8cd2540ee4263fa
srivstavapravesh <srivastavapravesh24@gmail.com>
2019-07-25 15:38:28 +0530

    second commit

---
+++ show.js
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+     </html>  
+  <head>
+  <title>list</title>
+     </head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="test.js"></script>
+     
+
+    <body>
+  
+<div class="container">
+  <h2>Basic Table</h2>
+  <p>The .table class adds basic styling (light padding and only horizontal dividers) to a table:</p>            
+  <table class="tableData">
+    <thead>
+      <tr>
+        <th>User Id</th>
+        <th>Id</th>
+        <th>Title</th>
+		<th>Body</th>
+      </tr>
+    </thead>
+<tbody id= "tableData"></tbody>
+      
+  </table>
+</div>
+
+</body>
+ 
+  
+  
